533b71a37434b9947c89b00aec78bf541dcede19
Maria Júlia <majuadelima@gmail.com>
2021-10-27 16:43:46 -0300

    WhileG3

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int idade=0;
+            int contador=1;
+            int qtdPessoas=0;
+
+           
+
+           while (contador <= 10)
+           {
+               contador++;
+               Console.WriteLine("Informe sua idade!");
+               idade = Convert.ToInt32(Console.ReadLine());
+           
+               if (idade >= 18)
+           {
+               qtdPessoas = qtdPessoas + 1;
+           }
+
+           }
+           Console.WriteLine($"{qtdPessoas} pessoas têm permissão para dirigir!");
+
+
+        }
+    }
+}
